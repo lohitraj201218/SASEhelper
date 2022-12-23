231a26b5a8ada0b777a2ca788653e0811ccaa9e9
lohitraj201218 <lohitraj201218@gmail.com>
2022-12-23 13:13:05 +0530

    Integration done with hemant's code

--- SASEhelper.py
+++ SASEhelper.py
@@ -6,8 +6,19 @@ import xml.etree.ElementTree as ET
 from lxml.etree import fromstring       # sudo apt-get install python3-lxml
 from optparse import OptionParser
 import os, sys
+import paramiko
 
-logging.basicConfig(filename='sasehelper.log', filemode='a',
+HOST     = "10.192.192.3"
+USERNAME = "admin"
+PASSWORD = "versa123"
+CMD_STARTER = ". /etc/profile.d/versa-profile.sh && "
+#VSH_CONNECT_CMD      = CMD_STARTER + "vsh connect vsmd"
+VSH_CONNECT_CMD  = "./versathon/getAllHistory.sh"
+GET_HISTORY_BY_ID_CMD = "./versathon/getHistoryById.sh "
+SHOW_HISTORY_ALL_CMD = "show saccess session history all"
+SHOW_HISTORY_ID_CMD  = "show saccess session history id "
+
+logging.basicConfig(filename='../reports/sasehelper.log', filemode='a',
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 logger=logging.getLogger()
@@ -19,12 +30,50 @@ headers = {
   'Authorization': 'Basic bG9oaXQxOnZlcnNhMTIz' # base64 encoded
 }
 
+def getRecentId(client):
+    #execute command
+    _stdin, _stdout,_stderr = client.exec_command(VSH_CONNECT_CMD)
+    cmd_exec_str = "command executing ==> " + VSH_CONNECT_CMD
+    print(cmd_exec_str)
+    logger.info(cmd_exec_str)
+    if _stderr != "":
+        output = _stdout.read().decode()
+        #print(output)
+        valueStr = output.split("Total entries : ",1)[1]
+        value = int(valueStr.split("\r",1)[0])
+        logger.debug(f"RecentHistoryID {value}")
+        return value
+    else:
+        ''''print(_stdout.read().decode())
+        return 2
+        _stdin, _stdout,_stderr = client.exec_command(SHOW_HISTORY_ALL_CMD)
+        if _stderr != "":
+            print(_stderr.read().decode())
+            return -1
+        else:
+            print(_stdout.read().decode())
+            return 4'''
+
+def getReport(client, startId, lastId, filename):
+    reportFile = open(filename, "w")
+    for i in range(startId + 1, lastId + 1):
+        CMD_ID = SHOW_HISTORY_ID_CMD + str(i)
+        _stdin, _stdout,_stderr = client.exec_command(GET_HISTORY_BY_ID_CMD + str(i))
+        cmd_exec_str = "command executing ==> " + CMD_ID + "\n"
+        print(cmd_exec_str)
+        logger.info(cmd_exec_str)
+        reportFile.write(cmd_exec_str.replace("^M", "" ))
+        if _stderr != "":
+            output = _stdout.read().decode()
+            #print(output)
+            reportFile.write(output.replace("^M", "\n" ))
+    reportFile.close()
 
 def writeReport(p):
     VTY_HISTORY_PATH = "http://10.192.200.11/reports/SASEhelper-history.log"
     LOG_URL          = "http://10.192.200.11/reports/sasehelper.log"
 
-    report = r"SASEhelper-report.html"
+    report = r"../reports/SASEhelper-report.html"
 
     try:
         rpt = open(report, 'w')
@@ -470,6 +519,15 @@ def main():
     org  = c_options.enterprise
     user = c_options.user
 
+    # connect to VOS
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(HOST, username=USERNAME, password=PASSWORD)
+    cmd_conn_str = 'connected to HOST ==> ' + HOST
+
+    # get recent id
+    startId = getRecentId(client)
+
     p = portal(fqdn)
 
     p.discover(fqdn, org, user)
@@ -503,6 +561,15 @@ def main():
             logger.debug("============== %s : %s ===============",
                          gw.getName(), gw.getTpasswd())
 
+    # get last ID
+    lastId = getRecentId(client)
+
+    # generate history report
+    filename = "../reports/SASEhelper-history.log"
+    getReport(client, startId, lastId, filename)
+    client.close()
+
+    #generate report
     writeReport(p)
 
 if __name__ == '__main__':
